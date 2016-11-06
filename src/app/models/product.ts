bfdd08244ec14d0a356e030976aee1c82dd1cb27
pauldubois777 <pauldubois777@users.noreply.github.com>
2016-11-06 07:21:42 -0600

    Added model Product and Initial Inventory object.

---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export class Product{
+  constructor(
+    public description: string,
+    public cost: number,
+    public qty: number
+  ) { }
+}
---
+++ src/app/shared/initial-inventory.ts
@@ -0,0 +1,6 @@
+import { Product } from '../models/product';
+export var INITIAL_INVENTORY: Array<Product> = [
+  new Product('Cola', 1, 20),
+  new Product('Chips', .5, 25),
+  new Product('Candy', .65, 30),
+];
