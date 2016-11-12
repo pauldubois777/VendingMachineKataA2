2561ddcf26fb76b648b1e04955f5323791a99554
pauldubois777 <pauldubois777@users.noreply.github.com>
2016-11-12 13:07:43 -0600

    Inserted Coins Service: First test for insertCoin

--- src/app/inserted-coins/inserted-coins.service.spec.ts
+++ src/app/inserted-coins/inserted-coins.service.spec.ts
@@ -5,17 +5,39 @@ import { BankService } from '../bank/bank.service';
 import { CoinsEnum } from '../shared/coins.enum';
 import { CoinReturnService } from '../coin-return/coin-return.service';
 import { InitialBankCoins } from '../bank/initial-bank-coins';
+import { MessageDisplayService } from '../message-display/message-display.service';
 
 let service: InsertedCoinsService;
 let bankService: BankService;
 let coinReturnService: CoinReturnService;
 let initialBankCoins: InitialBankCoins;
+let messageDisplayService: MessageDisplayService;
 
 describe('Service: Inserted Coins', () => {
   beforeEach(() => {
+    messageDisplayService = new MessageDisplayService();
+    spyOn(messageDisplayService, 'setDisplayBalance');
+
     initialBankCoins = new InitialBankCoins();
     coinReturnService = new CoinReturnService;
     bankService = new BankService(initialBankCoins, coinReturnService);
-    service = new InsertedCoinsService(coinReturnService, bankService);
+    service = new InsertedCoinsService(coinReturnService, bankService, messageDisplayService);
+  });
+
+  it('after creation should have initial coin quantities of 0 and value of 0', () => {
+    expect(service.getCoinBalance(CoinsEnum.NICKLE)).toEqual(0);
+    expect(service.getCoinBalance(CoinsEnum.DIME)).toEqual(0);
+    expect(service.getCoinBalance(CoinsEnum.QUARTER)).toEqual(0);
+    expect(service.ValueInCents).toEqual(0);
+  });
+
+  describe('insertCoin increases coin balance and value, and properly calls MessageDisplay DisplayBalance for', () => {
+    it('Nickle', () => {
+      service.insertCoin(CoinsEnum.NICKLE);
+
+      expect(service.getCoinBalance(CoinsEnum.NICKLE)).toEqual(1);
+      expect(messageDisplayService.setDisplayBalance).toHaveBeenCalled();
+      expect(messageDisplayService.setDisplayBalance).toHaveBeenCalledWith(.05);
+    });
   });
 });
--- src/app/inserted-coins/inserted-coins.service.ts
+++ src/app/inserted-coins/inserted-coins.service.ts
@@ -4,12 +4,24 @@ import { CoinsBalance } from '../models/coins-balance';
 import { CoinsEnum } from '../shared/coins.enum';
 import { BankService } from '../bank/bank.service';
 import { CoinReturnService } from '../coin-return/coin-return.service';
+import { MessageDisplayService } from '../message-display/message-display.service';
 
 @Injectable()
 export class InsertedCoinsService extends CoinsBalance {
 
-  constructor(private coinReturnService: CoinReturnService, private bankService: BankService) {
+  constructor(
+    private coinReturnService: CoinReturnService,
+    private bankService: BankService,
+    private messageDisplayService: MessageDisplayService) {
+
     super(0, 0, 0);
+  }
+
+  insertCoin(coinEnum: CoinsEnum) {
+    if (coinEnum === CoinsEnum.NICKLE) {
+      this.addCoin(coinEnum);
+      this.messageDisplayService.setDisplayBalance(this.ValueInCents / 100);
+    }
   }
 
 }
