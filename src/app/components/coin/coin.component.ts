1215d29552c02846ba30a27b69a979862340be96
pauldubois777 <pauldubois777@users.noreply.github.com>
2016-11-18 09:16:07 -0600

    Coin Component: Refactored so the coin displays it's own text, using a helper, instead of the parent compnent telling it what text to use for a coin.

--- src/app/components/coin/coin.component.ts
+++ src/app/components/coin/coin.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { CoinsEnum } from '../../shared/coins.enum';
+import { getCoinText } from '../../shared/helpers';
 
 @Component({
   selector: 'vmk-coin',
-  template: `<button (click)='onCoinClick()'>{{coinText}}</button>`,
+  template: `<button (click)='onCoinClick()'>{{coinText()}}</button>`,
   styles: []
 })
 export class CoinComponent implements OnInit {
   @Input() coinEnum: CoinsEnum = CoinsEnum.UNKNOWN;
-  @Input() coinText: string = 'Unknown';
   @Output() coinClicked = new EventEmitter<CoinsEnum>();
 
   constructor() {
@@ -20,6 +20,10 @@ export class CoinComponent implements OnInit {
 
   onCoinClick() {
     this.coinClicked.emit(this.coinEnum);
+  }
+
+  coinText(): string {
+    return getCoinText(this.coinEnum);
   }
 
 }
--- src/app/shared/helpers.ts
+++ src/app/shared/helpers.ts
@@ -1,3 +1,5 @@
+import { CoinsEnum } from './coins.enum';
+
 export function formatPrice(priceInCents: number): string {
   let formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -6,4 +8,20 @@ export function formatPrice(priceInCents: number): string {
   });
 
   return formatter.format(priceInCents / 100);
+}
+
+export function getCoinText(coinEnum: CoinsEnum): string {
+
+  switch (coinEnum) {
+    case CoinsEnum.PENNY:
+      return '1c';
+    case CoinsEnum.NICKLE:
+      return '5c';
+    case CoinsEnum.DIME:
+      return '10c';
+    case CoinsEnum.QUARTER:
+      return '25c';
+    default:
+      return 'Unknown';
+  }
 }
