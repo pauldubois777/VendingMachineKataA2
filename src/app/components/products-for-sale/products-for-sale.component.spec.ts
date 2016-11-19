411a0eb0f894b1f1cc06d60f6ccb28c4edf44719
pauldubois777 <pauldubois777@users.noreply.github.com>
2016-11-19 08:35:06 -0600

    Products For Sale: Added dependencies to test

--- src/app/components/products-for-sale/products-for-sale.component.spec.ts
+++ src/app/components/products-for-sale/products-for-sale.component.spec.ts
@@ -4,13 +4,30 @@ import { TestBed, async } from '@angular/core/testing';
 import { ProductsForSaleComponent } from './products-for-sale.component';
 import { InventoryService } from '../../services/inventory/inventory.service';
 import { InitialInventory } from '../../services/inventory/initial-inventory';
+import { MessageService } from '../../services/message/message.service';
+import { InsertedCoinsService } from '../../services/inserted-coins/inserted-coins.service';
+import { BankService } from '../../services/bank/bank.service';
+import { CoinReturnService } from '../../services/coin-return/coin-return.service';
+import { InitialBankCoins } from '../../services/bank/initial-bank-coins';
+import { PurchaseService } from '../../services/purchase/purchase.service';
 
 describe('Component: ProductsForSale', () => {
   it('should create an instance', () => {
-    let initialInventory = new InitialInventory();
-    let inventoryService = new InventoryService(initialInventory);
+    let messageService: MessageService;
+    let inventoryService: InventoryService;
+    let insertedCoinsService: InsertedCoinsService;
+    let bankService: BankService;
+    let coinReturnService: CoinReturnService;
+    let purchaseService: PurchaseService;
 
-    let component = new ProductsForSaleComponent(inventoryService);
+    messageService = new MessageService();
+    inventoryService = new InventoryService(new InitialInventory());
+    coinReturnService = new CoinReturnService();
+    bankService = new BankService(new InitialBankCoins(), coinReturnService);
+    insertedCoinsService = new InsertedCoinsService(coinReturnService, bankService, messageService);
+    purchaseService = new PurchaseService(messageService, inventoryService, insertedCoinsService);
+
+    let component = new ProductsForSaleComponent(inventoryService, purchaseService);
     expect(component).toBeTruthy();
   });
 });
