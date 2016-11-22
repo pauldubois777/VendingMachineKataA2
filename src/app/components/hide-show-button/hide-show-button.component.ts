255fa18e71ed9ad21cb8708e78ad4f0c9309b955
pauldubois777 <pauldubois777@users.noreply.github.com>
2016-11-22 11:17:27 -0600

    Give color to the Hide Show button

--- src/app/components/hide-show-button/hide-show-button.component.ts
+++ src/app/components/hide-show-button/hide-show-button.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'vmk-hide-show-button',
-  template: `<button class='btn btn-xs' (click)='toggleHideShow()'>{{butonText()}}</button>`,
+  template: `<button class='btn btn-xs btn-info' (click)='toggleHideShow()'>{{butonText()}}</button>`,
   styles: []
 })
 export class HideShowButtonComponent implements OnInit {
